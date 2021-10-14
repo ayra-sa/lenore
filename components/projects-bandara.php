dc5815d093ed6d1d1585d06cec334e2f5b2d1225
ayra-saputra <78393955+ayra-sa@users.noreply.github.com>
2021-10-14 11:14:25 +0700

    up

--- components/projects-bandara.php
+++ components/projects-bandara.php
@@ -32,7 +32,7 @@
         </div>
     <div class="col-lg-12 d-flex desc-projects">
       <div class="col-lg-7 col-md-7">
-        <h2 class="title">Plaza Premium Lounge</h2>
+        <h2 class="title">Plaza Premium Mega Lounge - Bandara Soekarno Hatta Terminal 3</h2>
         <p>We deliver the lighting products come true with the best idea. That's what partnership are for.</p>
       </div>
       <div class="col-lg-5 col-md-5 text-end m-auto">
